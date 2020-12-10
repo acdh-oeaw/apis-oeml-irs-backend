8891d2bdb624d3397f613c1129ed41e4f45b5b9a
Matthias Schlögl <m.schloegl@gmail.com>
2020-12-10 17:51:22 +0100

    initial commit

---
+++ apis/settings/dev.py
@@ -0,0 +1,47 @@
+from .base import *
+import re
+import dj_database_url
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '^mm-24*i-6iecm7c@z9l+7%^ns^4g^z!8=dgffg4ulggr-4=1%'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+REDMINE_ID = "14590"
+APIS_LIST_VIEWS_ALLOWED = True
+APIS_DETAIL_VIEWS_ALLOWED = True
+FEATURED_COLLECTION_NAME = "FEATURED"
+MAIN_TEXT_NAME = "ÖBL Haupttext"
+BIRTH_REL_NAME = "place of birth"
+DEATH_REL_NAME = "place of death"
+APIS_BASE_URI = "https://apis.acdh.oeaw.ac.at/"
+APIS_OEBL_BIO_COLLECTION = "ÖBL Biographie"
+
+
+ALLOWED_HOSTS = re.sub(r'https?://', '', os.environ.get('GITLAB_ENVIRONMENT_URL', os.environ.get('ALLOWED_HOSTS', 'localhost'))).split(',')
+# You need to allow '10.0.0.0/8' for service health checks.
+ALLOWED_CIDR_NETS = ['10.0.0.0/8', '127.0.0.0/8']
+
+
+SECRET_KEY = 'd3j@454545()(/)@zlck/6dsaf*#sdfsaf*#sadflj/6dsfk-11$)d6ixcvjsdfsdf&-u35#ayi'
+DEBUG = True
+DEV_VERSION = False
+
+
+DATABASES = {}
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+MAIN_TEXT_NAME = "ÖBL Haupttext"
+
+LANGUAGE_CODE = "de"
+
+
+#APIS_COMPONENTS = ['deep learning']
+
+#APIS_BLAZEGRAPH = ('https://blazegraph.herkules.arz.oeaw.ac.at/metaphactory-play/sparql', 'metaphactory-play', 'KQCsD24treDY')
+
+
+APIS_RELATIONS_FILTER_EXCLUDE += ['annotation', 'annotation_set_relation']
+
