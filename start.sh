f4d5c60975d81873fdef3db9251a938e410c43ae
Matthias Schlögl <m.schloegl@gmail.com>
2021-01-26 14:31:41 +0100

    changes in supervisord

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 python manage.py migrate --settings=apis.settings.dev
+supervisord -c celery_config/celery.conf
 supervisorctl -c celery_config/celery.conf start all
 gunicorn apis.wsgi
