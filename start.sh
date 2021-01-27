c4bf5fef84f333b607de3d16dec65f911c166469
Matthias Schlögl <m.schloegl@gmail.com>
2021-01-27 08:27:54 +0100

    moving logs to tmp directory

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 python manage.py migrate --settings=apis.settings.dev
+mkdir /tmp/celery
 supervisord -c celery_config/celery.conf
 supervisorctl -c celery_config/celery.conf start all
 gunicorn apis.wsgi
